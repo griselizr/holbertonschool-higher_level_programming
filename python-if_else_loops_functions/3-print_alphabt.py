9c069257c2b0262af5a05b81b30fe605699dfc03
griselizr <griseliz.r.c15@gmail.com>
2022-09-08 19:24:21 -0400

    except letters q & e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for letters in range(ord('a'), ord('z')+1):
-  if i != 101 and i != 113:
-        print(chr(i), end="",)
-        
+  if letters != 101 and letters != 113:
+        print(chr(letters), end="",)
