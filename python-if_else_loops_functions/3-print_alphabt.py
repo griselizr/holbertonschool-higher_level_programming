bf6992adfecbedb977198b529c77509fed9b451b
griselizr <griseliz.r.c15@gmail.com>
2022-09-08 18:42:06 -0400

    spacing

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for letters in range(ord('a'), ord('z')+1):
-    if letters in [101,103]:
+    if letters in [101, 103]:
         continue
     print("{:c}".format(letters), end="")
-    
