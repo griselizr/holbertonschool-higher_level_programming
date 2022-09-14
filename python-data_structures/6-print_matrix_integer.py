386cf459527a47860cf8ea31823e02d6e1cf4c6d
griselizr <git-commit-address>
2022-09-14 19:45:44 -0400

    indent

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for rows in range(len(matrix)):
-        for columns in range(len(rows)):
+    for rows in matrix:
+        for columns in range(0, len(rows)):
             print("{:d}".format(rows[columns]), end="")
             if columns != len(rows) - 1:
                 print("", end="")
