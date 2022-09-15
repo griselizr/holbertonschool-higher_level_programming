1de5a2dfc0791483c4f523ceacf6d841e49fbbd3
griselizr <git-commit-address>
2022-09-14 22:06:11 -0400

    syntax error

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) >= 2:
         new = (tuple_b[0], tuple_b[1])
     if len(tuple_b) == 1:
-        new= (tuple_b[0], 0)
+        new = (tuple_b[0], 0)
     if len(tuple_b) == 0:
         new = (0, 0)
     if len(tuple_a) >= 2:
