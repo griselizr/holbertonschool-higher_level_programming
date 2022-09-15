653d467a3127c78e686fe2c407032a97ff87cf2b
griselizr <git-commit-address>
2022-09-14 21:34:08 -0400

    fix syntax

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     multiples = []
-    for numbers in my_list
+    for numbers in my_list:
         if numbers % 2 == 0:
             multiples.append(True)
         else:
