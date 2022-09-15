2963404d9674b4a707bad076ee502eb865e79bb2
griselizr <git-commit-address>
2022-09-15 17:56:51 -0400

    spacing

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return list(map(lambda rep : rep if rep != search else replace, my_list))
+    return list(map(lambda rep: rep if rep != search else replace, my_list))
