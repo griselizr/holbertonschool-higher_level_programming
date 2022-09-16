70bc798290b575318ff0395aed114f13e364e6e2
griselizr <git-commit-address>
2022-09-16 18:37:55 -0400

    fix typo

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     first = 0
-    for idx in range(x):
+    for idx in range(0, x):
         try:
-            print("{:d}".format(my_list), end="")
+            print("{:d}".format(my_list[idx]), end="")
             first += 1
-        except (ValueError, TypeError):
+        except (TypeError, ValueError):
             continue
-    print()
+    print("")
     return (first)
