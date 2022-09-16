16b99a043c735b34b368358d3a3ec622293ffb70
griselizr <git-commit-address>
2022-09-16 18:20:48 -0400

    new file

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    first = 0
+    for idx in range(x)
+        try:
+            print("{:d}".format(my_list), end="")
+            first += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return (first)
