5b69473e1ff58295c9a2bf0ed961106e62c946aa
griselizr <git-commit-address>
2022-09-16 21:01:15 -0400

    indentation

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,17 +2,16 @@
 def list_division(my_list_1, my_list_2, list_length):
      new_list = []
      for i in range(list_length)
-        divides = 0
-        try:
+     try:
             divides = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             divides = 0
         except ZeroDivisionError:
             print("division by 0")
-            divides = 0
         except IndexError:
             print("out of range")
+            divides = 0
         finally:
             new_list.append(divides)
     return new_list
