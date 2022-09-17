8495b70a805dc86e2a32ff32a56fecefa7fa1ae8
griselizr <git-commit-address>
2022-09-16 23:58:23 -0400

    indent

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-    return None
+        return None
 
     score = max(a_dictionary.values(), default=None)
     for keys, val in a a_dictionary.items():
