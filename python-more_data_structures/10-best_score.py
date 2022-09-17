e88c7e77283f476916c748cebae545f632056462
griselizr <git-commit-address>
2022-09-16 23:56:06 -0400

    code

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None
+    return None
+
+    score = max(a_dictionary.values(), default=None)
+    for keys, val in a a_dictionary.items():
+        if val == score:
+            return keys
