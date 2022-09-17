8ed98972ac946dc37a2c334be77f20b1b232959e
griselizr <git-commit-address>
2022-09-16 21:48:25 -0400

    code

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        total = fct(*args)
+        return(total)
+    except Exception as mistake:
+        print("Exception: {}".format(mistake), file=sys.stderr)
