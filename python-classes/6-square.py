432735f356be240a2b6498f1afad8f581e87f953
griselizr <git-commit-address>
2022-09-23 01:57:03 -0400

    new file

---
+++ python-classes/6-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Building a  class Square """
+
+
+class Square:
+    """ Define the Square """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not type(value) is tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
