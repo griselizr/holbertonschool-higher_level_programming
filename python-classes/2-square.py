e40e395b45504cc541b5271cc125e20a968c15b3
griselizr <git-commit-address>
2022-09-23 00:54:21 -0400

    spacing

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-
-
-
+"""creating a Square"""
 
 
 class Square:
-    """ Defining the square """
+    """Define the Square"""
 
     def __init__(self, size=0):
         """private instance size = int"""
-        
+
         if not instance(size, int):
-        raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-        raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
