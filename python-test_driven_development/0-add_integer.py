491aff3c582a8bc3bfdc1c6e83fb9935127bf904
griselizr <git-commit-address>
2022-09-23 17:11:52 -0400

    add more code in line

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,9 +4,9 @@
 
 def add_integer(a, b=98):
     """ Define add integer  a + b"""
-    if not type(a) is int and is not type(a) is float:
+    if not type(a) is int and not type(a) is float:
         raise TypeError("a must be an integer")
-    if not type(b) is int and is not type(b) is float:
+    if not type(b) is int and  not type(b) is float:
         raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
