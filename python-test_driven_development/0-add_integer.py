eeb7b8ae4ad80a037bc9ee95cd3937d283b2bdc0
griselizr <git-commit-address>
2022-09-23 15:39:00 -0400

    new file

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if type(a) is not int or float:
+            raise TypeError("a must be an integer")
+    if type(b) is not int or float:
+            raise TypeError("b must be an integer")
+    if type(a) is a float:
+            a = int(a)
+    if type(b) is a float:
+            b = int(b)
+            return a + b
