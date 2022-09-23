6acc2ebde947828d4c2fe69b8bef85311593352a
griselizr <git-commit-address>
2022-09-23 02:00:20 -0400

    new file

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -40,3 +40,4 @@ class Square:
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
+                print("")
