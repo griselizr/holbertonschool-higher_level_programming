80e13a0c7b948d84354a71d0123ae9902f2247a9
griselizr <git-commit-address>
2022-09-23 01:38:47 -0400

    newfile

---
+++ python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" create a class Square """
+
+
+class Square:
+    """ Define the Square """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+#!/usr/bin/python3
+""" Module contains class Square """
+
+
+class Square:
+    """ Defines Square """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
