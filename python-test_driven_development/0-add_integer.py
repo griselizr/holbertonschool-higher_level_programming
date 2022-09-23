7d1a16add10c702a365bbe05fe3008bb263577a7
griselizr <git-commit-address>
2022-09-23 16:20:47 -0400

    add doc comment

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+        """ sum of a and B"""
+
+        
     if type(a) is not int or float:
             raise TypeError("a must be an integer")
     if type(b) is not int or float:
