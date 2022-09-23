b9c21011eb9bfad943ab8b4282f9105feb7a0418
griselizr <git-commit-address>
2022-09-23 16:42:25 -0400

    indentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,14 +2,14 @@
 """ add two integers """
 
 
-        def add_integer(a, b=98):
-                """ add integer  a + b"""
-                if not type(a) is int:
-                        raise TypeError("a must be an integer")
-                if not type(b) is int:
-                        raise TypeError("b must be an integer")
-                if type(a) is a float:
-                        a = int(a)
-                if type(b) is a float:
-                        b = int(b)
-                return a + b
+def add_integer(a, b=98):
+""" add integer  a + b"""
+    if not type(a) is int:
+        raise TypeError("a must be an integer")
+    if not type(b) is int:
+        raise TypeError("b must be an integer")
+    if type(a) is a float:
+        a = int(a)
+    if type(b) is a float:
+        b = int(b)
+    return a + b
