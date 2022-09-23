40dd4401dbd69ac2af9d1159775aacdcaf67e49f
griselizr <git-commit-address>
2022-09-23 16:31:05 -0400

    indentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+""" add two integers """
+
+
 def add_integer(a, b=98):
-        """ sum of a and B"""
-
-
-    if type(a) is not int or float:
+        """ sum of a and b"""
+    if type(a) is not int:
         raise TypeError("a must be an integer")
-    if type(b) is not int or float:
+    if type(b) is not int:
         raise TypeError("b must be an integer")
     if type(a) is a float:
         a = int(a)
