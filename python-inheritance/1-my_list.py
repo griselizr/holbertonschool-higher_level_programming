efd638e654eb766645e40a753ff74a93d3a5a6f5
griselizr <git-commit-address>
2022-09-29 16:33:57 -0400

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-""" 
-    Defines a class MyList that inherits from list 
-"""
+""" Defines a class MyList that inherits from list """
 
 
 class MyList(list):
-    """ 
-        Defines Mylist that inherits from list 
-    """
+    """ Defines Mylist that inherits from list """
+
+
     def print_sorted(self):
-    """ 
-        Prints the list, but sorted in ascending order
-    """
+    """ Prints the list, but sorted in ascending order"""
 
 
         if issubclass(MyList, list)
-        print(sorted(self))
+            print(sorted(self))
