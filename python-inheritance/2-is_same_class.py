aacae653eaa42f6b61df510106a15a3e9865e828
griselizr <git-commit-address>
2022-09-29 18:43:42 -0400

    fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,7 +6,7 @@ def is_same_class(obj, a_class):
     """ Define the same class as a class """
 
 
-        if type obj is a_class
+        if type obj == a_class
             return True
         elif
             return False
