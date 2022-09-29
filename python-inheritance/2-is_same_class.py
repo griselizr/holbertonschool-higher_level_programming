58627b7b4478f1738a259ee25211bf3e6c1c167e
griselizr <git-commit-address>
2022-09-29 17:15:56 -0400

    new file

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" creating an instance of a class or otherwise """
+
+
+def is_same_class(obj, a_class):
+    """ Define the same class as a class """
+    if is_same_class is an instance
+        print(True)
+    else
+        print(False)
