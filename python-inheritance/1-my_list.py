ebebfeccde8a9e0f82818af9d1486c6d3a525bf9
griselizr <git-commit-address>
2022-09-28 22:25:38 -0400

    new file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Creates a class MyList that inherits from list """
+
+
+class MyList(list):
+    """ Define Mylist that inherits from list """
+    def print_sorted(self):
+    """ With public instance method that prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
