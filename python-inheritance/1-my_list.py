4f8398e2cf8e1ebeb55cd528269499d9906eb088
griselizr <git-commit-address>
2022-09-29 13:35:05 -0400

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ 
-Defines a class MyList that inherits from list 
+    Defines a class MyList that inherits from list 
 """
 
 
 class MyList(list):
     """ 
-    Defines Mylist that inherits from list 
+        Defines Mylist that inherits from list 
     """
     def print_sorted(self):
     """ 
