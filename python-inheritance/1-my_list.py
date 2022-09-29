514db8e34e6e10bdb64008802358173314aafeec
griselizr <git-commit-address>
2022-09-28 23:37:32 -0400

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,4 +6,7 @@ class MyList(list):
     """ Define Mylist that inherits from list """
     def print_sorted(self):
     """ With public instance method that prints the list, but sorted (ascending sort)"""
+
+    
+        if issubclass(MyList, list)
         print(sorted(self))
