b757d1da1179f1d41c3ddfa5df0a1c7ff9433c6f
griselizr <git-commit-address>
2022-09-28 23:39:18 -0400

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-""" Creates a class MyList that inherits from list """
+""" 
+Creates a class MyList that inherits from list 
+"""
 
 
 class MyList(list):
-    """ Define Mylist that inherits from list """
+    """ 
+    Define Mylist that inherits from list 
+    """
     def print_sorted(self):
-    """ With public instance method that prints the list, but sorted (ascending sort)"""
+    """ 
+    With public instance method that prints the list, but sorted (ascending sort)
+    """
 
-    
+
         if issubclass(MyList, list)
         print(sorted(self))
