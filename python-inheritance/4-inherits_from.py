793f8364dbb7c5be5954b6ff14bd6183205e0e47
griselizr <git-commit-address>
2022-09-29 22:17:54 -0400

    spcing

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,11 +3,10 @@
 
 
 def inherits_from(obj, a_class):
-    """ Verify if an object is an inherited of a class
-    obj:the object 
-    a_class (type): The class to pait the obj
-    Returns:
-    If obj is an inherited instance of a_class  is True,
+    """
+    Verify if an object is an inherited of a class
+    obj:the object , a_class (type): the class to pair the obj
+    Returns: If obj is an inherited instance of a_class is True,
     Otherwise is False.
     """
 
