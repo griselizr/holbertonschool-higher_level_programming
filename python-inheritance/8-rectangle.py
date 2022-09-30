d916672608a220ca23fb1bf1d49b3d356c719d8d
griselizr <git-commit-address>
2022-09-30 00:10:19 -0400

    spacing

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Creates a class Rectangle that inherits from BaseGeometry"""
+"""
+class rectangle and a BaseGeometry
+"""
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Rectangle(__import__('7-base_geometry').BaseGeometry):
-    """Defines a rectangle class"""
-
+class Rectangle(BaseGeometry):
+    """Defines a rectangle"""
     def __init__(self, width, height):
-        """initialize rectangle
-        width (int): The width of the new Rectangle.
-        height (int): The height of the new Rectangle.
-        """
+        """instantiation of the rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
