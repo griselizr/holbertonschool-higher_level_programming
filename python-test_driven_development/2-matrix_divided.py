89addaf705c2dfc0bcaec28bfb9f47f85632a810
griselizr <git-commit-address>
2022-09-30 00:31:10 -0400

    doc

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,32 +1,29 @@
-#!/usr/bin/python3
-""" matrix_divided divides all elements of matrix """
+!/usr/bin/python3
+"""
+class matrix divided
+"""
 
 
 def matrix_divided(matrix, div):
-    """ Divides all elements of a matrix by "div"
-    verify list is int/float
-     if each list are the same size
-    checks if "div" is an int/float or is 0
-    """
-    matrix1 = "matrix must be a matrix (list of lists) of integers/floats"
-    matrix2 = "Each row of the matrix must have the same size"
-    result_matrix = []
-
-    if not isinstance(div, (int, float)):
+    """Divides all elements in the matrix by div"""
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    size = None
+    for l in matrix:
+        if type(l) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(l)
+        elif size != len(l):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in l:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    for lists in matrix:
-        if len(lists) != len(matrix[0]):
-            raise TypeError(matrix2)
-        another_list = []
-        for items in lists:
-            if not isinstance(items, (int, float)):
-                raise TypeError(matrix1)
-            else:
-                another_list.append(round(items / div, 2))
-        result_matrix.append(another_list)
-
-    return result_matrix
+    return [[round(i / div, 2) for i in l] for l in matrix]
