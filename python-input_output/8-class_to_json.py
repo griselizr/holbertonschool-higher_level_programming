960a5e007efad68768e0e8489ffaab78d320b837
griselizr <git-commit-address>
2022-09-30 21:50:26 -0400

    new file

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Creates JSON serialization of an object to return 
+a dictionary with list, dictionary, string, integer and boolean
+"""
+
+
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
