04c8073a810a43f662f86018109cb4eac57b0146
griselizr <git-commit-address>
2022-09-30 20:42:53 -0400

    rm whitespace

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,5 @@ def read_file(filename=""):
     """ Read the text inside the file and print"""
     
     with open(filename, "r", encoding="utf-8") as f:
-              for line in f:
-                  print(line, end='') 
-    
+        for line in f:
+            print(line, end='')
