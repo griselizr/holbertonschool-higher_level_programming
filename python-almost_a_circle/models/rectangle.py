83c10d882e53c08c74d16b6a04b17c21b98257e7
griselizr <git-commit-address>
2022-10-05 17:28:00 -0400

    whitespace

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Creates a class Rectangle"""
 
-from models.base import  Base
+from models.base import Base
+
 
 class Rectangle(Base):
-    """Defines a class rectangle""" 
+    """Defines a class rectangle"""
 
-   
-    def __init__(self, width, height, x=0, y=0, id=None): 
+    def __init__(self, width, height, x=0, y=0, id=None):
         """ initialization of private attributes """
         super().__init__(id)
 
@@ -15,42 +15,42 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-    
+
     @property
     def width(self):
         """get wight"""
         return self.__width
-    
+
     @width.setter
     def width(self, width):
         """set width"""
         self.__width = width
-    
+
     @property
     def height(self):
         """get height """
         return self.__height
-    
+
     @height.setter
     def height(self, height):
         """set height """
         self.__height = height
-    
+
     @property
     def x(self):
         """get x """
         return self.__x
-    
+
     @x.setter
     def x(self, x):
         """ set x """
         self.__x = x
-    
+
     @property
     def y(self):
         """ get y """
         return self.__y
-    
+
     @y.setter
     def y(self, y):
         """set y"""
