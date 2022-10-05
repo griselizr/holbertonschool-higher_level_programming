fcf09044495ea92101c9616b258de9672f4175e6
griselizr <git-commit-address>
2022-10-05 17:12:03 -0400

    fix syntax

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(Base):
    
     def __init__(self, width, height, x=0, y=0, id=None): 
         """ initialization of private attributes """
-        super().__init__(self, id)
+        super().__init__(id)
 
         self.width = width
         self.height = height
