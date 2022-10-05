55cafac37d044528144ed8c88fe05701c45b59b5
griselizr <git-commit-address>
2022-10-05 14:15:19 -0400

    new file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,74 +1,52 @@
-#!/usr/bin/python3
-"""creates a square class."""
-
-
-from models.base import Base
+#!/usr/bin/python
+"""Creates a class Rectangle"""
 
 
 class Rectangle(Base):
-    """Defines a Rectangle"""
-
-    def __init__(self, size, x=0, y=0, id=None):
-        """ private instances"""
-        """ class constructor """
+"""Defines a class rectangle"""    
+    def __init__(self, width, height, x=0, y=0, id=None): 
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(size, size, x, y, id)
-        
-@property
+    
+    @property
     def width(self):
-        """ width getter """
+        """get wight"""
         return self.__width
-
+    
     @width.setter
-    def width(self, value):
-        """ width setter """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
+    def width(self, ___width = width):
+        """set width"""
+        return self.___width
+    
     @property
     def height(self):
-        """ height getter """
+        """get height """
         return self.__height
-
+    
     @height.setter
-    def height(self, value):
-        """ height setter """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
+    def height(self, __height = height)
+        """set height """
+        return self.__height
+    
     @property
     def x(self):
-        """ x getter """
+        """get x """
         return self.__x
-
+    
     @x.setter
-    def x(self, value):
-        """ x setter """
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
+    def x(self, __x = x)
+        """ set x """
+        return self.__x
+    
     @property
-    def y(self):
-        """ y getter """
+    def y(self)
+        """ get y """
         return self.__y
-
+    
     @y.setter
-    def y(self, value):
-        """ y setter """
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
+    def y(self,__y = y)
+        """ set y"""
+        return self.__y
+    
