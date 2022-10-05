c4b7c5a1c979c68bcbce7c0bdb0c40f1cb70f359
griselizr <git-commit-address>
2022-10-04 23:18:33 -0400

    new file

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Class Base"""
+
+import json
+
+
+class Base:
+    """ Defines a base model"""
+        #private class attribute
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
