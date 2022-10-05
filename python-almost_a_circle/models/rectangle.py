421ac8c113537c47101e6de36652bdb7936a9c0e
griselizr <git-commit-address>
2022-10-05 14:54:04 -0400

    add a doc

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,6 @@ class Rectangle(Base):
    
     def __init__(self, width, height, x=0, y=0, id=None): 
         """ initialization of private attributes """
-          super(Base, id, self).__init__()
         self.__width = width
         self.__height = height
         self.__x = x
