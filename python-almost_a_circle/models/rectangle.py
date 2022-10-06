794d50c7a5def99c3e0551860cbc3a155d433ac4
griselizr <git-commit-address>
2022-10-05 22:39:28 -0400

    task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -41,7 +41,7 @@ class Rectangle(Base):
         """set height """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         else:
             self.__height = value
