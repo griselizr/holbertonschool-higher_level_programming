3a1c55054b227bb157a9b3e5c7f9dc43ddd1bd41
griselizr <git-commit-address>
2022-10-05 22:42:08 -0400

    task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -27,7 +27,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width  must be > 0")
+            raise ValueError("width must be > 0")
         else:
             self.__width = value
 
