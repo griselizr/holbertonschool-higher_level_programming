66c8187e033365934903270220fdab6f491ada6f
griselizr <git-commit-address>
2022-10-07 00:17:27 -0400

    update display

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,7 @@ class Rectangle(Base):
         """ prints height and width starting from 0"""
         for i in range(0, self.height):
             for side in range(self.x):
-                print(" ", end="")
+                print("", end="")
             for other in range(0, self.width):
                 print("#", end="")
                 """ prints newline"""
