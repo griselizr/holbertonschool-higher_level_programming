be314f53fb97fb3d5bebc87f006bd8b8082da2ce
griselizr <git-commit-address>
2022-11-06 20:02:52 -0400

    fix syntax msql server user password & permission

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates the MySQL server user user_0d_1. 
 CREATE USER IF NOT EXISTS
-GRANT *.* TO user_0d_1@localhost;
- SET PASSWORD FOR user_0d_1'@'localhost = PASSWORD(user_0d_1_pwd);
+SET PASSWORD FOR user_0d_1@localhost = PASSWORD(user_0d_1_pwd);
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
