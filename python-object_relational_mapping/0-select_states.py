6d1746cbb7deefede40aa65b4fe310fa1faa20aa
griselizr <git-commit-address>
2022-11-09 01:33:29 -0400

    new file upload

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' lists all states from the database hbtn_0e_0_usa:
+
+    Your script should take 3 arguments: mysql username, mysql password and database name'''
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    connect = MySQLdb.connect(host="localhost",
+                              port=3306,
+                              user=sys.argv[1],
+                              passwd=sys.argv[2],
+                              dv=sys.argv[3],
+                              charset="utf8")
+    present = connect.cursor()
+    present.execute("SELECT * FROM states ORDER by states.id ASC")
+    query_row = present.fetchall()
+    for rows in query_row:
+        print(rows)
+    present.close()
+    connect.close()
