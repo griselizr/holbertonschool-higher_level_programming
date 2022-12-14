4c360d06f9fa8ea80c9a748e097a37629eb5d5c7
griselizr <git-commit-address>
2022-12-13 23:16:38 -0400

    prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const lang_mess = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+let idx;
+
+for (idx = 0; idx < lang_mess.lenght; ++idx) {
+    console.log(lang_mess[idx]);
+}
