2392319276b8e07bcd892e6fc2abaa54f3ad8947
griselizr <git-commit-address>
2022-12-15 23:41:46 -0400

    title of movie

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $.get('https://swapi-api.hbtn.io/api/films/?format=json', function
+        (data, status) {
+        if (status === 'success') {
+            const movies = data.results;
+            for (const movies of data.results) {
+                $('UL#list_movies').append(`<li>$(movie.title}</>`);
+            }
+        }
+    });
+});
